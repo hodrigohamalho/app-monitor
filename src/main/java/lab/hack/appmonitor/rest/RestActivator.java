7a27c69eb8222b2f8825622442865c52f4a3cd7d
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2015-08-07 04:00:58 -0300

    fixed REST setup: missing inheritance

--- src/main/java/lab/hack/appmonitor/rest/RestActivator.java
+++ src/main/java/lab/hack/appmonitor/rest/RestActivator.java
@@ -1,6 +1,7 @@
 package lab.hack.appmonitor.rest;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class RestActivator {}
+public class RestActivator extends Application{}
