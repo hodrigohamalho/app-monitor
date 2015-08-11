bcfe797387c29ea749f90ef1b1537b972957fbec
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2015-08-11 02:21:47 -0300

    Added option to save server on view

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,15 +1,24 @@
 (function(){
 	var app = angular.module('app-monitor', []);
 
-	app.controller('AppController', [ '$http', function($http){
+	app.controller('ServerController', [ '$scope', '$http', function($scope, $http){
 		var self = this;
+		$scope.server = {};
 		
-		$http.get('api/servers').success(function(data){
-			console.log("data: "+data);
-			self.servers = data;
-		}).error(function(error){
-			console.log("Error: "+error);
-		});
+		$scope.list = function(){
+			$http.get('api/servers').then(function(serversList){
+				$scope.servers = serversList.data;
+			});
+		};
+		
+		$scope.save = function(){
+			$http.post('api/servers', $scope.server).then(function(){
+				$scope.servers.unshift($scope.server);
+				$scope.server = {};
+			});
+		};
+		
+		$scope.list();
 		
 	}]);
 
