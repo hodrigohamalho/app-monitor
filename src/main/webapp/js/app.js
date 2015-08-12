d720fc9ab069d494313b020a672094648029f78d
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2015-08-11 22:51:10 -0300

    include of apps working

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,33 @@
 (function(){
 	var app = angular.module('app-monitor', ['ui.bootstrap']);
 	
+	app.controller('AppModalCtrl', function ($scope, $http, $modalInstance, app, $log) {
+
+		$scope.app = app;
+
+		$scope.save = function(){
+			if ($scope.app.id != null){
+				// UPDATE
+				$http.put('api/apps', $scope.app).then(function(){
+					$log.info("app updated!");
+				});
+			}else{
+				// SAVE
+				$http.post('api/apps', $scope.app).then(function(){
+					$log.info("app saved!");
+				});
+			}
+			
+			$modalInstance.close($scope.app);
+		};
+
+		$scope.cancel = function () {
+			$log.info("Cancel");
+			$modalInstance.dismiss('cancel');
+		};
+		  
+	});
+	
 	app.controller('ServerModalCtrl', function ($scope, $http, $modalInstance, server, $log) {
 
 		$scope.server = server;
@@ -31,6 +58,7 @@
 	app.controller('ServerController', function($scope, $http, $modal, $log){
 		var self = this;
 		$scope.server = {};
+		$scope.app = {};
 
 		$scope.list = function(){
 			$http.get('api/servers').then(function(serversList){
@@ -59,6 +87,33 @@
 		      resolve: {
 		          server: function () {
 		            return $scope.server;
+		          }
+		        }
+		    });
+
+			modalInstance.result.then(function (data) {
+				$scope.list();
+				$log.info("Callback modal: "+data);
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
+		
+		$scope.appModal = function (app, serverId) {
+			debugger;
+			if (app != null){
+				$scope.app = app;
+			}else{
+				$scope.app.server = { "id": serverId };
+			}
+				
+		    var modalInstance = $modal.open({
+		      animation: true,
+		      templateUrl: 'appModalForm.html',
+		      controller: 'AppModalCtrl',
+		      resolve: {
+		          app: function () {
+		            return $scope.app;
 		          }
 		        }
 		    });
