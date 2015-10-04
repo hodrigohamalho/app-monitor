1bde058d2ed1268501e3661e964c559ab9f1cc7d
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2015-10-03 21:58:34 -0300

    CORS enabled

---
+++ api/src/main/java/lab/hack/appmonitor/util/CORSFilterRequest.java
@@ -0,0 +1,31 @@
+package lab.hack.appmonitor.util;
+
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@PreMatching
+public class CORSFilterRequest implements ContainerRequestFilter {
+
+	@Inject
+    private Logger log;
+
+    @Override
+    public void filter( ContainerRequestContext requestCtx ) throws IOException {
+        // When HttpMethod comes as OPTIONS, just acknowledge that it accepts...
+        if ( requestCtx.getRequest().getMethod().equals( "OPTIONS" ) ) {
+            log.info( "HTTP Method (OPTIONS) - Detected!" );
+
+            // Just send a OK signal back to the browser
+            requestCtx.abortWith( Response.status( Response.Status.OK ).build() );
+        }
+    }
+}
---
+++ api/src/main/java/lab/hack/appmonitor/util/CORSFilterResponse.java
@@ -0,0 +1,20 @@
+package lab.hack.appmonitor.util;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+ 
+@Provider
+public class CORSFilterResponse implements ContainerResponseFilter {
+
+	@Override
+    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
+        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+    }
+	
+}
